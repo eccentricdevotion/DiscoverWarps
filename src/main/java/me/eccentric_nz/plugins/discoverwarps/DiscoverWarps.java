2ab351b51d7db4f55f028ece653c917a6f789c77
eccentric devotion <eccentricdevotion@home.nz>
2012-12-21 00:26:18 +1300

    Set up Vault if configured

--- src/main/java/me/eccentric_nz/plugins/discoverwarps/DiscoverWarps.java
+++ src/main/java/me/eccentric_nz/plugins/discoverwarps/DiscoverWarps.java
@@ -3,18 +3,24 @@ package me.eccentric_nz.plugins.discoverwarps;
 import java.io.File;
 import java.io.IOException;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import net.milkbowl.vault.Vault;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DiscoverWarps extends JavaPlugin {
 
-    protected static DiscoverWarps plugin;
+    protected DiscoverWarps plugin;
     DiscoverWarpsDatabase service = DiscoverWarpsDatabase.getInstance();
     private DiscoverWarpsCommands commando;
     PluginManager pm = Bukkit.getServer().getPluginManager();
     DiscoverWarpsPlateListener plateListener = new DiscoverWarpsPlateListener(plugin);
     DiscoverWarpsProtectionListener protectionListener = new DiscoverWarpsProtectionListener(plugin);
     DiscoverWarpsExplodeListener explodeListener = new DiscoverWarpsExplodeListener(plugin);
+    private Vault vault;
+    public Economy economy;
 
     @Override
     public void onDisable() {
@@ -39,7 +45,10 @@ public class DiscoverWarps extends JavaPlugin {
             getDataFolder().setWritable(true);
             getDataFolder().setExecutable(true);
         }
-
+        // add allow_buying to config if missing
+        if (!getConfig().contains("allow_buying")) {
+            getConfig().set("allow_buying", true);
+        }
         try {
             String path = getDataFolder() + File.separator + "DiscoverWarps.db";
             service.setConnection(path);
@@ -59,6 +68,39 @@ public class DiscoverWarps extends JavaPlugin {
         } catch (IOException e) {
             // Failed to submit the stats :-(
         }
+        if (getConfig().getBoolean("allow_buying")) {
+            if (!setupVault()) {
+                pm.disablePlugin(this);
+                return;
+            }
+            setupEconomy();
+        }
+    }
+
+    private boolean setupVault() {
+        Plugin x = pm.getPlugin("Vault");
+        if (x != null && x instanceof Vault) {
+            vault = (Vault) x;
+            return true;
+        } else {
+            System.err.println("Vault is required for economy, but wasn't found!");
+            System.err.println("Download it from http://dev.bukkit.org/server-mods/vault/");
+            System.err.println("Disabling plugin.");
+            return false;
+        }
+    }
+
+    //Loading economy API from Vault
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        economy = rsp.getProvider();
+        return economy != null;
     }
 
     public void debug(Object o) {
