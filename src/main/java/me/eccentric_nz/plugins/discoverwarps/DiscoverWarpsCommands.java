23a0e3d0137513521a508a1c20915a205e61f7c7
eccentric devotion <eccentricdevotion@home.nz>
2012-12-21 00:28:34 +1300

    Add commands to toggle config 'allow_buying', set discoverplate to auto, add a cost to a discoverplate, buy a discoverplate location, set a discover[plate as respawn location. If allow_buying is configured, then list the price for the list command

--- src/main/java/me/eccentric_nz/plugins/discoverwarps/DiscoverWarpsCommands.java
+++ src/main/java/me/eccentric_nz/plugins/discoverwarps/DiscoverWarpsCommands.java
@@ -36,9 +36,13 @@ public class DiscoverWarpsCommands implements CommandExecutor {
         admincmds.add("delete");
         admincmds.add("enable");
         admincmds.add("disable");
+        admincmds.add("auto");
+        admincmds.add("cost");
+        admincmds.add("allow_buying");
         this.usercmds = new ArrayList<String>();
         usercmds.add("tp");
         usercmds.add("list");
+        usercmds.add("buy");
     }
 
     @Override
@@ -54,6 +58,13 @@ public class DiscoverWarpsCommands implements CommandExecutor {
             if (admincmds.contains(args[0])) {
                 if (!sender.hasPermission("discoverwarps.admin")) {
                     sender.sendMessage(DiscoverWarpsConstants.MY_PLUGIN_NAME + "You do not have permission to run that command!");
+                    return true;
+                }
+                if (args[0].equalsIgnoreCase("allow_buying")) {
+                    boolean bool = !plugin.getConfig().getBoolean("allow_buying");
+                    plugin.getConfig().set("allow_buying", bool);
+                    sender.sendMessage(DiscoverWarpsConstants.MY_PLUGIN_NAME + "allow_buying was set to: " + bool);
+                    plugin.saveConfig();
                     return true;
                 }
                 if (args.length < 2) {
@@ -168,6 +179,55 @@ public class DiscoverWarpsCommands implements CommandExecutor {
                         plugin.debug("Could not delete discover plate, " + e);
                     }
                 }
+                if (args[0].equalsIgnoreCase("auto")) {
+                    try {
+                        Connection connection = service.getConnection();
+                        Statement statement = connection.createStatement();
+                        String queryAuto = "SELECT name, auto FROM discoverwarps WHERE name = '" + args[1] + "'";
+                        ResultSet rsAuto = statement.executeQuery(queryAuto);
+                        // check name is valid
+                        if (rsAuto.next()) {
+                            int auto = (rsAuto.getInt("auto") == 1) ? 0 : 1;
+                            String bool = (auto == 1) ? "true" : "false";
+                            String queryDel = "UPDATE discoverwarps SET auto = " + auto + " WHERE name = '" + args[1] + "'";
+                            statement.executeUpdate(queryDel);
+                            sender.sendMessage(DiscoverWarpsConstants.MY_PLUGIN_NAME + "DiscoverPlate " + args[1] + " auto-discovery is " + bool + "!");
+                            return true;
+                        } else {
+                            sender.sendMessage(DiscoverWarpsConstants.MY_PLUGIN_NAME + "Couldn't find a DiscoverPlate with that name!");
+                            return true;
+                        }
+                    } catch (SQLException e) {
+                        plugin.debug("Could not delete discover plate, " + e);
+                    }
+                }
+                if (args[0].equalsIgnoreCase("cost")) {
+                    try {
+                        Connection connection = service.getConnection();
+                        Statement statement = connection.createStatement();
+                        String queryCost = "SELECT name FROM discoverwarps WHERE name = '" + args[1] + "'";
+                        ResultSet rsCost = statement.executeQuery(queryCost);
+                        // check name is valid
+                        if (rsCost.isBeforeFirst()) {
+                            int cost;
+                            try {
+                                cost = Integer.parseInt(args[2]);
+                            } catch (NumberFormatException nfe) {
+                                sender.sendMessage(DiscoverWarpsConstants.MY_PLUGIN_NAME + "The last argument must be a number!");
+                                return true;
+                            }
+                            String queryDel = "UPDATE discoverwarps SET cost = " + cost + " WHERE name = '" + args[1] + "'";
+                            statement.executeUpdate(queryDel);
+                            sender.sendMessage(DiscoverWarpsConstants.MY_PLUGIN_NAME + "DiscoverPlate " + args[1] + " now costs " + cost + " to buy!");
+                            return true;
+                        } else {
+                            sender.sendMessage(DiscoverWarpsConstants.MY_PLUGIN_NAME + "Couldn't find a DiscoverPlate with that name!");
+                            return true;
+                        }
+                    } catch (SQLException e) {
+                        plugin.debug("Could not delete discover plate, " + e);
+                    }
+                }
             }
             if (usercmds.contains(args[0])) {
                 if (!sender.hasPermission("discoverwarps.use")) {
@@ -189,7 +249,7 @@ public class DiscoverWarpsCommands implements CommandExecutor {
                                 visited = Arrays.asList(rsVisited.getString("visited").split(","));
                             }
                         }
-                        String queryList = "SELECT id, name FROM discoverwarps WHERE enabled = 1";
+                        String queryList = "SELECT id, name, auto, cost FROM discoverwarps WHERE enabled = 1";
                         ResultSet rsList = statement.executeQuery(queryList);
                         // check name is valid
                         if (rsList.isBeforeFirst()) {
@@ -199,8 +259,16 @@ public class DiscoverWarpsCommands implements CommandExecutor {
                             String discovered;
                             while (rsList.next()) {
                                 discovered = (visited.contains(rsList.getString("id"))) ? ChatColor.GREEN + "VISITED" : ChatColor.RED + "NOT VISITED";
+                                String status = (rsList.getBoolean("auto")) ? ChatColor.BLUE + "AUTO" : discovered;
                                 String warp = rsList.getString("name");
-                                sender.sendMessage(i + ". " + warp + " " + discovered);
+                                String cost = "";
+                                if (plugin.getConfig().getBoolean("allow_buying") && !visited.contains(rsList.getString("id"))) {
+                                    int amount = rsList.getInt("cost");
+                                    if (amount > 0) {
+                                        cost = ChatColor.RESET + " [" + plugin.economy.format(amount) + "]";
+                                    }
+                                }
+                                sender.sendMessage(i + ". " + warp + " " + status + cost);
                                 i++;
                             }
                             sender.sendMessage("------------");
@@ -265,6 +333,105 @@ public class DiscoverWarpsCommands implements CommandExecutor {
                         plugin.debug("Could not delete discover plate, " + e);
                     }
                 }
+                if (args[0].equalsIgnoreCase("buy")) {
+                    if (!plugin.getConfig().getBoolean("allow_buying")) {
+                        sender.sendMessage(DiscoverWarpsConstants.MY_PLUGIN_NAME + "You are not allowed to buy DiscoverWarps on this server!");
+                        return true;
+                    }
+                    Player player;
+                    if (sender instanceof Player) {
+                        player = (Player) sender;
+                    } else {
+                        sender.sendMessage(DiscoverWarpsConstants.MY_PLUGIN_NAME + "This command requires a player!");
+                        return true;
+                    }
+                    if (args.length < 2) {
+                        sender.sendMessage(DiscoverWarpsConstants.MY_PLUGIN_NAME + "You need to supply a warp name!");
+                        return false;
+                    }
+                    try {
+                        Connection connection = service.getConnection();
+                        Statement statement = connection.createStatement();
+                        String queryBuy = "SELECT * FROM discoverwarps WHERE name = '" + args[1] + "'";
+                        ResultSet rsBuy = statement.executeQuery(queryBuy);
+                        // check name is valid
+                        if (rsBuy.next()) {
+                            boolean firstplate = true;
+                            double cost = rsBuy.getDouble("cost");
+                            if (cost <= 0) {
+                                sender.sendMessage(DiscoverWarpsConstants.MY_PLUGIN_NAME + "You cannot buy the location of DiscoverPlate " + args[1] + "!");
+                                return true;
+                            }
+                            String p = player.getName();
+                            String id = rsBuy.getString("id");
+                            String queryDiscover = "";
+                            // check whether they have visited this plate before
+                            String queryPlayer = "SELECT * FROM players WHERE player = '" + p + "'";
+                            ResultSet rsPlayer = statement.executeQuery(queryPlayer);
+                            if (rsPlayer.next()) {
+                                firstplate = false;
+                                String data = rsPlayer.getString("visited");
+                                String[] visited = data.split(",");
+                                if (Arrays.asList(visited).contains(id)) {
+                                    sender.sendMessage(DiscoverWarpsConstants.MY_PLUGIN_NAME + "You have already discovered " + args[1] + "!");
+                                    return true;
+                                }
+                                queryDiscover = "UPDATE players SET visited = '" + data + "," + id + "' WHERE player = '" + p + "'";
+                            }
+                            if (firstplate == true) {
+                                queryDiscover = "INSERT INTO players (player, visited) VALUES ('" + p + "','" + id + "')";
+                            }
+                            statement.executeUpdate(queryDiscover);
+                            plugin.economy.withdrawPlayer(p, cost);
+                            player.sendMessage(DiscoverWarpsConstants.MY_PLUGIN_NAME + "You bought the DiscoverPlate location " + args[1] + " for " + cost);
+                            return true;
+                        } else {
+                            sender.sendMessage(DiscoverWarpsConstants.MY_PLUGIN_NAME + "Couldn't find a DiscoverPlate with that name!");
+                            return true;
+                        }
+                    } catch (SQLException e) {
+                        plugin.debug("Could not buy discover plate, " + e);
+                    }
+                }
+                if (args[0].equalsIgnoreCase("spawn")) {
+                    if (!plugin.getConfig().getBoolean("allow_set_spawn")) {
+                        sender.sendMessage(DiscoverWarpsConstants.MY_PLUGIN_NAME + "You are not allowed to set DiscoverWarp spawns on this server!");
+                        return true;
+                    }
+                    Player player;
+                    if (sender instanceof Player) {
+                        player = (Player) sender;
+                    } else {
+                        sender.sendMessage(DiscoverWarpsConstants.MY_PLUGIN_NAME + "This command requires a player!");
+                        return true;
+                    }
+                    if (args.length < 2) {
+                        sender.sendMessage(DiscoverWarpsConstants.MY_PLUGIN_NAME + "You need to supply a warp name!");
+                        return false;
+                    }
+                    try {
+                        Connection connection = service.getConnection();
+                        Statement statement = connection.createStatement();
+                        String queryLoc = "SELECT * FROM discoverwarps WHERE name = '" + args[1] + "'";
+                        ResultSet rsLoc = statement.executeQuery(queryLoc);
+                        // check name is valid
+                        if (rsLoc.next()) {
+                            World w = plugin.getServer().getWorld(rsLoc.getString("world"));
+                            int x = rsLoc.getInt("x");
+                            int y = rsLoc.getInt("y");
+                            int z = rsLoc.getInt("z");;
+                            Location loc = new Location(w, x, y, z);
+                            player.setBedSpawnLocation(loc, true);
+                            player.sendMessage(DiscoverWarpsConstants.MY_PLUGIN_NAME + "Your spawn location was set to " + args[1]);
+                            return true;
+                        } else {
+                            sender.sendMessage(DiscoverWarpsConstants.MY_PLUGIN_NAME + "Couldn't find a DiscoverPlate with that name!");
+                            return true;
+                        }
+                    } catch (SQLException e) {
+                        plugin.debug("Could not set bed spawn location, " + e);
+                    }
+                }
             }
         }
         return false;
@@ -298,9 +465,9 @@ public class DiscoverWarpsCommands implements CommandExecutor {
             @Override
             public void run() {
                 thePlayer.teleport(theLocation);
-//                if (plugin.getConfig().getBoolean("no_damage")) {
-//                    thePlayer.setNoDamageTicks(plugin.getConfig().getInt("no_damage_time") * 20);
-//                }
+                if (plugin.getConfig().getBoolean("no_damage")) {
+                    thePlayer.setNoDamageTicks(plugin.getConfig().getInt("no_damage_time") * 20);
+                }
                 if (thePlayer.getGameMode() == GameMode.CREATIVE || (allowFlight && crossWorlds)) {
                     thePlayer.setAllowFlight(true);
                 }
