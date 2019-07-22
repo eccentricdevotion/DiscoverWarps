ea0235c98fd9660d54dcf1849d1e93640c8482a8
eccentricdevotion <eccentricdevotion@gmail.com>
2019-07-23 09:13:50 +1200

    Make the plugin enable if WorldEdit is not installed

--- src/main/java/me/eccentric_nz/discoverwarps/DiscoverWarpsCommands.java
+++ src/main/java/me/eccentric_nz/discoverwarps/DiscoverWarpsCommands.java
@@ -1,12 +1,5 @@
 package me.eccentric_nz.discoverwarps;
 
-import com.sk89q.worldedit.bukkit.BukkitWorld;
-import com.sk89q.worldedit.math.BlockVector3;
-import com.sk89q.worldguard.WorldGuard;
-import com.sk89q.worldguard.internal.platform.WorldGuardPlatform;
-import com.sk89q.worldguard.protection.ApplicableRegionSet;
-import com.sk89q.worldguard.protection.managers.RegionManager;
-import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -22,7 +15,6 @@ import org.bukkit.inventory.ItemStack;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
 public class DiscoverWarpsCommands implements CommandExecutor {
@@ -118,28 +110,7 @@ public class DiscoverWarpsCommands implements CommandExecutor {
                         String region_name = "";
                         // check if the plate is inside a WorldGuard region
                         if (plugin.getConfig().getBoolean("worldguard_regions") && plugin.pm.isPluginEnabled("WorldGuard")) {
-                            WorldGuardPlatform wg = WorldGuard.getInstance().getPlatform();
-                            RegionManager rm = wg.getRegionContainer().get(new BukkitWorld(l.getWorld()));
-                            BlockVector3 vector = BlockVector3.at(l.getX(), l.getY(), l.getZ());
-                            ApplicableRegionSet ars = rm.getApplicableRegions(vector);
-                            if (ars.size() > 0) {
-                                LinkedList<String> parentNames = new LinkedList<>();
-                                LinkedList<String> regions = new LinkedList<>();
-                                for (ProtectedRegion pr : ars) {
-                                    String id = pr.getId();
-                                    regions.add(id);
-                                    ProtectedRegion parent = pr.getParent();
-                                    while (parent != null) {
-                                        parentNames.add(parent.getId());
-                                        parent = parent.getParent();
-                                    }
-                                }
-                                parentNames.forEach((name) -> {
-                                    regions.remove(name);
-                                });
-                                region_name = regions.getFirst();
-                                sender.sendMessage(plugin.getLocalisedName() + String.format(plugin.getConfig().getString("localisation.region_found"), region_name));
-                            }
+                            region_name = DiscoverWarpsWorldGuardUtilities.isPlateInRegion(plugin, l, sender);
                         }
                         Statement statement = null;
                         ResultSet rsName = null;
---
+++ src/main/java/me/eccentric_nz/discoverwarps/DiscoverWarpsWorldGuardUtilities.java
@@ -0,0 +1,44 @@
+package me.eccentric_nz.discoverwarps;
+
+import com.sk89q.worldedit.bukkit.BukkitWorld;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.internal.platform.WorldGuardPlatform;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import org.bukkit.Location;
+import org.bukkit.command.CommandSender;
+
+import java.util.LinkedList;
+
+public class DiscoverWarpsWorldGuardUtilities {
+
+    public static String isPlateInRegion(DiscoverWarps plugin, Location l, CommandSender sender) {
+        // check if the plate is inside a WorldGuard region
+        String region_name = "";
+        WorldGuardPlatform wg = WorldGuard.getInstance().getPlatform();
+        RegionManager rm = wg.getRegionContainer().get(new BukkitWorld(l.getWorld()));
+        BlockVector3 vector = BlockVector3.at(l.getX(), l.getY(), l.getZ());
+        ApplicableRegionSet ars = rm.getApplicableRegions(vector);
+        if (ars.size() > 0) {
+            LinkedList<String> parentNames = new LinkedList<>();
+            LinkedList<String> regions = new LinkedList<>();
+            for (ProtectedRegion pr : ars) {
+                String id = pr.getId();
+                regions.add(id);
+                ProtectedRegion parent = pr.getParent();
+                while (parent != null) {
+                    parentNames.add(parent.getId());
+                    parent = parent.getParent();
+                }
+            }
+            parentNames.forEach((name) -> {
+                regions.remove(name);
+            });
+            region_name = regions.getFirst();
+            sender.sendMessage(plugin.getLocalisedName() + String.format(plugin.getConfig().getString("localisation.region_found"), region_name));
+        }
+        return region_name;
+    }
+}
